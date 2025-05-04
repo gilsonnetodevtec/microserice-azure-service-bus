2c627c13159a32b037eb951e7e25338e4d629eae
gilsonnetodevtec <gilsonnetodevtec@gmail.com>
2025-05-04 19:51:23 -0300

    feat: iniciando os dois projetos

---
+++ send/src/test/java/com/service_messenger/send/SendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service_messenger.send;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
